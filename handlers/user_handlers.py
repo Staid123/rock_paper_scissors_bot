5994fddf536ab079180a84acddc86b5f71e1ca34
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2023-07-20 12:34:42 +0300

    Изменена библиотека, поэтому костыль в user_handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,5 +1,6 @@
+from aiogram.filters.command import Command, CommandStart
+from aiogram import F
 from aiogram import Router
-from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message
 from keyboards.keyboards import game_kb, yes_no_kb
 from lexicon.lexicon_ru import LEXICON_RU
@@ -21,22 +22,40 @@ async def process_help_command(message: Message):
 
 
 # Этот хэндлер срабатывает на согласие пользователя играть в игру
-@router.message(Text(text=LEXICON_RU['yes_button']))
+@router.message(F.text == LEXICON_RU['yes_button'])
 async def process_yes_answer(message: Message):
     await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
 
 
 # Этот хэндлер срабатывает на отказ пользователя играть в игру
-@router.message(Text(text=LEXICON_RU['no_button']))
+@router.message(F.text == LEXICON_RU['no_button'])
 async def process_no_answer(message: Message):
     await message.answer(text=LEXICON_RU['no'])
 
 
-# Этот хэндлер срабатывает на любую из игровых кнопок
-@router.message(Text(text=[LEXICON_RU['rock'],
-                           LEXICON_RU['paper'],
-                           LEXICON_RU['scissors']]))
-async def process_game_button(message: Message):
+# Этот хэндлер срабатывает на кнопку 'Камень'
+@router.message(F.text == 'Камень')
+async def process_rock_button(message: Message):
+    bot_choice = get_bot_choice()
+    await message.answer(text=f'{LEXICON_RU["bot_choice"]} '
+                              f'- {LEXICON_RU[bot_choice]}')
+    winner = get_winner(message.text, bot_choice)
+    await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
+
+
+# Этот хэндлер срабатывает на кнопку 'Бумага'
+@router.message(F.text == 'Бумага')
+async def process_paper_button(message: Message):
+    bot_choice = get_bot_choice()
+    await message.answer(text=f'{LEXICON_RU["bot_choice"]} '
+                              f'- {LEXICON_RU[bot_choice]}')
+    winner = get_winner(message.text, bot_choice)
+    await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
+
+
+# Этот хэндлер срабатывает на кнопку 'Ножницы'
+@router.message(F.text == 'Ножницы')
+async def process_scissors_button(message: Message):
     bot_choice = get_bot_choice()
     await message.answer(text=f'{LEXICON_RU["bot_choice"]} '
                               f'- {LEXICON_RU[bot_choice]}')
